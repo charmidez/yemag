fdc87b64c45beb1cdf8b24be7a76bedb3cc87af9
charmidez <kodjoassiobo@protonmail.com>
2025-04-23 11:51:56 +0000

    Project Next LAncé

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <div className="">
-      
+    <div className="justify-center items-center flex flex-col h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+      <div className="btn bg-red-500 text-4xl text-white fond-bold p-10 rounded-4xl">
+        Cliquez ici pour commencer
+      </div>
     </div>
   );
 }
