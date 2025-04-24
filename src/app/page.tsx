fde904165692c3ca6713c688edc4244caf38a35c
charmidez <kodjoassiobo@protonmail.com>
2025-04-24 21:23:05 +0000

    Navbar construction start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import Navbar from "./navbar";
 
 
 export default function Home() {
   return (
-    <div className="justify-center items-center flex flex-col h-screen bg-gradient-to-r from-blue-500 to-purple-500">
-      <div className="btn bg-red-500 text-4xl text-white fond-bold p-10 rounded-4xl">
-        Cliquez ici pour commencer
-      </div>
-    </div>
+    <>
+    <Navbar/>
+    </>
   );
 }
