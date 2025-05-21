2ef7aaff571bc95319af8eed440a571fc1cb5c59
charmidez <kodjoassiobo@protonmail.com>
2025-05-21 23:42:23 +0000

    no output

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/out',
-  assetPrefix: '/out/'
+  //output: 'export',
+  //basePath: '/out',
+  //assetPrefix: '/out/'
 };
 
 //module.exports = nextConfig;
